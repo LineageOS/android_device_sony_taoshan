2256fba62566aa22c868bc1d4aad7de1983e7e07
Ying Wang <wangying@google.com>
2014-12-10 12:44:33 -0800

    taoshan: aapt: Set PRODUCT_AAPT_PREBUILT_DPI
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.
    
    Change-Id: Iba98af754c3397ddbb27287244db086cb4c43aa6

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREBUILT_DPI := hdpi tvdpi mdpi ldpi
 
 # Device specific overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
