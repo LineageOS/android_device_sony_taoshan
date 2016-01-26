33df6c351dcd2d52c2294b6100ea4e3f13010019
Ethan Chen <intervigil@gmail.com>
2016-01-25 17:24:35 -0800

    taoshan: Don't set BFQ scheduler until boot is complete
    Change-Id: Ib199faa253ce7981ff3b4f8d512f3eb11e95d902

--- product/init.mk
+++ product/init.mk
@@ -4,7 +4,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc
-
-# IO Scheduler
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.io.scheduler=bfq
