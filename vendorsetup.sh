fc00cb26e1ceb44f10afca37dccb6c291f200304
NoelMacwan <yulespencer@gmail.com>
2017-01-05 18:34:16 +0530

    taoshan: Rebrand device project for LineageOS
    Change-Id: Ic01f185d718fee2e36347f3cf3e12b5ee1aed2d5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_taoshan-eng
-add_lunch_combo cm_taoshan-userdebug
+add_lunch_combo lineage_taoshan-eng
+add_lunch_combo lineage_taoshan-userdebug
